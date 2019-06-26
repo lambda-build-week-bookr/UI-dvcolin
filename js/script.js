f9b1f2c89717f7c2a960058d2151b30008c8f299
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-26 15:15:57 -0700

    Added tabs and components for members on about page. Added event listener on buttons to display members

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+class aboutButton {
+    constructor(element) {
+        this.element = element;
+        
+        this.jobData = element.dataset.job;
+
+        if (this.jobData === 'all') {
+            this.members = document.querySelectorAll('.about__member');
+        } else {
+            this.members = document.querySelectorAll(`.about__member[data-job = '${this.jobData}']`);
+        }
+
+        this.members = Array.from(this.members).map(member => new TeamMember(member));
+
+        this.element.addEventListener('click', () => this.select());
+    }
+
+    select() {
+        const allMembers = document.querySelectorAll('.about__member');
+        allMembers.forEach(member => member.style.display = 'none');
+
+        this.members.forEach(member => member.active());
+
+
+    }
+}
+
+class TeamMember {
+    constructor(element) {
+        this.element = element;
+    }
+
+    active() {
+        this.element.style.display = 'block';
+    }
+}
 
 
 
@@ -6,16 +42,6 @@
 
 
 
+let aboutButtons = document.querySelectorAll('.about__button');
 
-
-
-
-
-
-
-
-
-
-
-
-let aboutMembers = document.querySelectorAll('.about__member');
+aboutButtons.forEach(button => new aboutButton(button));
